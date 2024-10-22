f0d72e3c92c0f636e6813181f954ee8c13f5e469
amin sk <aminskeh786@gmail.com>
2024-10-22 10:32:06 +0530

    all dependincs install & project setup and first server run

---
+++ src/app.ts
@@ -0,0 +1,40 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+//import cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+//import router from "./app/routes";
+// import notFound from "./app/middlewares/notFound";
+// import globalErrorHandler from "./app/middlewares/globalErrorhandler";
+
+const app: Application = express();
+
+// Parsers
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+
+    credentials: true, // Allow credentials to be sent
+  })
+);
+app.options("*", cors());
+// Application routes
+//app.use("/api/v1", router);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello Travel Tips & Destination Guides");
+});
+
+// Global Error
+//app.use(globalErrorHandler);
+
+//Not Found
+//app.use(notFound);
+
+export default app;
